cffbf3b7e0f915543c2d35651f1fffd5bb744c3c
Mohd Faiz Hasim <faizhasim@gmail.com>
2018-07-27 11:38:09 +0800

    Remove unnecessary debug messages

--- src/main/scala/com/faizhasim/lightningtalk/akkapersistence/LmsApp.scala
+++ src/main/scala/com/faizhasim/lightningtalk/akkapersistence/LmsApp.scala
@@ -1,17 +1,16 @@
 package com.faizhasim.lightningtalk.akkapersistence
 
+import TrainingCreditModel._
 import akka.actor.ActorSystem
 
 object LmsApp extends App {
-  import TrainingCreditModel._
-  println(akka.serialization.JavaSerializer)
   val system = ActorSystem("lms")
   val trainingCreditActor = system.actorOf(TrainingCreditActor.props())
 
   trainingCreditActor ! ResetTrainingCredit(50)
   trainingCreditActor ! AddTrainingCreditCoupon(TrainingCreditCoupon(10))
-  trainingCreditActor ! AddTrainingCreditCoupon(TrainingCreditCoupon(2))
   trainingCreditActor ! SpendTrainingCredit(4)
+  trainingCreditActor ! AddTrainingCreditCoupon(TrainingCreditCoupon(2))
   trainingCreditActor ! SpendTrainingCredit(22)
   trainingCreditActor ! SpendTrainingCredit(14)
   trainingCreditActor ! AddTrainingCreditCoupon(TrainingCreditCoupon(10))
@@ -19,3 +18,4 @@ object LmsApp extends App {
   Thread.sleep(5000)
   system.terminate()
 }
+
